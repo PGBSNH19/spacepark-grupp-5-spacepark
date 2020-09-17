869165bf234fe5dd8ee2f010d50ba1f8053d8e3b
Fredrika Eriksson <56867257+erifredrika@users.noreply.github.com>
2020-09-17 13:56:52 +0200

    added in contextfile

--- SpaceParkProject/SpaceParkBackend/Database/SpaceparkContext.cs
+++ SpaceParkProject/SpaceParkBackend/Database/SpaceparkContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SpaceParkBackend.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SpaceParkBackend.Database
 {
-    public class SpaceparkContext
+    public class SpaceparkContext : DbContext
     {
+        public virtual DbSet<Person> Persons { get; set; }
+        public virtual DbSet<Parkinglot> Parkinglots { get; set; }
+        public virtual DbSet<Spaceship> Spaceships { get; set; }
+
     }
 }
