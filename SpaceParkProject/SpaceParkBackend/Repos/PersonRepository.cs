4ff077c93ae04be82e5caf12c16d5c04555961bf
Fredrika Eriksson <56867257+erifredrika@users.noreply.github.com>
2020-09-21 15:23:45 +0200

    added some classes

---
+++ SpaceParkProject/SpaceParkBackend/Repos/PersonRepository.cs
@@ -0,0 +1,21 @@
+﻿using SpaceParkBackend.Database;
+using SpaceParkBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkBackend.Repos
+{
+    public class PersonRepository : IPersonRepository
+    {
+        public PersonRepository(SpaceparkContext spaceContext) : base(spaceContext)
+        {
+
+        }
+        public async Task<Person[]> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
